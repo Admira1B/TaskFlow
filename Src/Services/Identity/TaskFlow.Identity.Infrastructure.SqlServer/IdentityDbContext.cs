44b086e848195ec5b81c55d446ceae3e3866a222
Vlad Reizenbuk <vreizenbuk@mail.ru>
2025-12-12 17:04:19 +0700

    Added project references in Services, Added Identity DbContext.

---
+++ Src/Services/Identity/TaskFlow.Identity.Infrastructure.SqlServer/IdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Identity.Domain.Entities;
+
+namespace TaskFlow.Identity.Infrastructure.SqlServer {
+    public class IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+               : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options) {
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+        }
+    }
+}
